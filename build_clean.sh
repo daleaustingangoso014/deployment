397fe2147136025b6b75298ab8ce3caaea8f5bc0
unknown <daleaustingangoso014@gmail.com>
2021-12-28 18:22:21 +0800

    test webp

---
+++ build_clean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -rf asset-manifest.json images logo192.png manifest.json robots.txt static favicon.ico index.html logo512.png precache-manifest.b71e82c1d9cbee6363980934f1799f12.js service-worker.js videos
+rm -rf build_clean.sh
